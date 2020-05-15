20dd7aa79d971b1488820c20fd5d8cc9c53ab716
Rodolfo <chavesrodolfo@gmail.com>
2020-05-14 22:29:22 -0300

    security api

---
+++ src/main/java/io/github/chavesrodolfo/config/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package io.github.chavesrodolfo.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfiguration extends WebSecurityConfiguration {
+    
+}
